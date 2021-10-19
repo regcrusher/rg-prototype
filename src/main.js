e769fe9980d2139387887fd91e04debff179884e
Reggie Granados <rgranados@gmail.com>
2021-10-18 21:36:17 -0400

    Add Vuex

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from "vue";
+import Vuex from "vuex";
 import App from "./App.vue";
 import "./index.css";
 
 Vue.config.productionTip = false;
+
+Vue.use(Vuex);
 
 new Vue({
   render: (h) => h(App),
