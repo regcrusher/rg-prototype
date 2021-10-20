31561af8321c5840037cc6897a1b21976f16f4e5
Reggie Granados <rgranados@gmail.com>
2021-10-19 21:03:26 -0400

    Update aria label

--- src/components/Movie.vue
+++ src/components/Movie.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="sm:w-1/2 md:w-1/2 lg:w-1/3 ">
     <div class="card  border-2 mx-2 mb-4">
-      <a :href="movie.link.url">
+      <a :href="movie.link.url" :aria-label="movie.display_title">
         <img
           v-if="movie.multimedia"
           class="rounded-t-lg motion-safe:hover:scale-110"
@@ -17,7 +17,9 @@
       </a>
       <div class="pt-2 px-2 pb-6">
         <div class="card__title text-xl mt-2 pb-1">
-          <a :href="movie.link.url">{{ movie.display_title }} </a>
+          <a :href="movie.link.url" :aria-label="movie.display_title"
+            >{{ movie.display_title }}
+          </a>
         </div>
         <p class="font-normal text-gray-700 mb-3">{{ movie.summary_short }}</p>
       </div>
