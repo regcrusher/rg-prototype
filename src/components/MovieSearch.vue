89d8202bcf3ff16ac4570a914af1c587b97895ca
Reggie Granados <rgranados@gmail.com>
2021-10-19 21:03:40 -0400

    Change submit behavior

--- src/components/MovieSearch.vue
+++ src/components/MovieSearch.vue
@@ -1,16 +1,17 @@
 <template>
-  <form class="w-full max-w-sm my-6">
+  <form class="w-full mx-auto max-w-sm my-6">
     <div class="flex items-center border-b border-teal-500 py-2">
       <input
-        class="appearance-none bg-transparent border-none w-full mr-3 py-1 px-2 leading-tight focus:outline-none"
+        class="appearance-none bg-transparent border-none w-full mr-3 py-1 px-2  focus:outline-none"
         type="text"
         v-model="searchTerm"
         placeholder="Enter your search term"
         aria-label="Movie name"
+        @keyup.enter="searchMovies"
       />
       <button
         class="flex-shrink-0 bg-black-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-2 py-1 px-2 rounded"
-        type="button"
+        type="submit"
         v-on:click="searchMovies"
       >
         Search Movies
@@ -31,7 +32,8 @@ export default {
     },
   },
   methods: {
-    searchMovies() {
+    searchMovies(e) {
+      e.preventDefault();
       this.$emit("searchMovies", this.searchTerm);
     },
   },
